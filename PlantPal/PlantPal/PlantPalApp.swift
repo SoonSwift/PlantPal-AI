bec7782141c21308471f7ca46b8759a83a18633a
Marcin <mar.dytko@gmail.com>
2024-07-07 10:32:37 +0200

    Project

---
+++ PlantPal/PlantPal/PlantPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantPalApp.swift
+//  PlantPal
+//
+//  Created by Marcin Dytko on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
