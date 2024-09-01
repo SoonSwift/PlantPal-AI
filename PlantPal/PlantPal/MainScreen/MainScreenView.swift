4261ff5d372cb3e566c36254bd3aa61f5476fbc8
SoonSwift <110138009+SoonSwift@users.noreply.github.com>
2024-09-01 09:54:08 +0200

    New views

--- PlantPal/PlantPal/MainScreen/MainScreenView.swift
+++ PlantPal/PlantPal/MainScreen/MainScreenView.swift
@@ -12,11 +12,73 @@ struct MainScreenView: View {
     
     var body: some View {
         MainStack {
-            VStack {
-                Text("MainScreen")
-                Button("Delete") {
-                    viewModel.deleteKey()
+            
+            switch viewModel.state {
+            case .loading:
+                EmptyView()
+            case .empty:
+                
+                VStack {
+                    PalText(text: "Recent scans", fontType: .regular, size: .extraLarge)
+                    
+                    Spacer()
+                    Image(.emptyBox)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.bottom, 24)
+                    
+                    PalText(text: "You don't have any scnas yet",
+                            fontType: .regular, size: .medium)
+                    .padding(.bottom, 8)
+                    
+                    PalButton(title: "Scan") {
+                        
+                    }
+                    Spacer()
+                    Spacer()
+                    
+                    Button("Delete") {
+                        viewModel.deleteKey()
+                    }
                 }
+                
+            case .loaded(let displayInfo):
+                VStack {
+                    ScrollView {
+                        PalText(text: "Recent scans", 
+                                fontType: .regular, size: .extraLarge)
+                            .padding(.top)
+                        
+                        VStack(alignment: .leading) {
+                            ForEach(displayInfo.list, id: \.id) { plant in
+                                HStack {
+                                    Image(plant.image)
+                                        .resizable()
+                                        .frame(width: 75, height: 75)
+                                    
+                                    VStack(alignment: .leading) {
+                                        PalText(text: plant.name,
+                                                fontType: .bold, size: .large)
+                                        .padding(.bottom, 2)
+                                        
+                                        PalText(text: plant.familyName,
+                                                fontType: .regular, size: .small)
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                
+                                PalDivider(color: .mainView)
+                            }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                    }
+                    Spacer()
+                    PalButton(title: "Scan") {
+                    }
+                    .padding(.bottom, 20)
+                }
+                .edgesIgnoringSafeArea(.bottom)
             }
         }
     }
--- PlantPal/PlantPal/MainScreen/MainScreenViewModel.swift
+++ PlantPal/PlantPal/MainScreen/MainScreenViewModel.swift
@@ -6,9 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 class MainScreenViewModel: ObservableObject {
+    struct PalntItem: Identifiable {
+        let image = "Plant"
+        let id = UUID()
+        let name: String
+        let familyName: String
+    }
+    
+    struct DisplayInfo {
+        let list: [PalntItem]
+    }
+    
+    enum State {
+        case loading
+        case empty
+        case loaded(DisplayInfo)
+    }
+    
     private let apiKeyService: any ApiKeyServiceInterface
+    
+    @Published var state: State = .loaded(DisplayInfo(list: [PalntItem(name: "dwadwa", 
+                                                                       familyName: "wawawa"), 
+                                                             PalntItem(name: "wdada2121",
+                                                                       familyName: "dwadwa2")]))
     
     init(apiKeyService: any ApiKeyServiceInterface) {
         self.apiKeyService = apiKeyService
